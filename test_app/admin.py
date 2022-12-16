aa65836395a1a159c267df9e1552b8fbbed25057
unknown <nikolrusik@gmail.com>
2022-12-16 16:06:01 +0300

    Register models for admin

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from test_app import models
 
-# Register your models here.
+
+@admin.register(models.TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+
+
+@admin.register(models.TestQuestionsModel)
+class TestQuestionsModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'quest', 'test']
+
+
+@admin.register(models.AnswersQuestionModel)
+class AnswersQuestionModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'answer', 'quest', 'is_success']
